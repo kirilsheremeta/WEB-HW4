69cd8aa19452f7a663e44a8be0475777693a1f75
Kiril Sheremeta <120675223+kirilsheremeta@users.noreply.github.com>
2023-04-15 21:15:22 +0300

    Update main.py

--- front-init/main.py
+++ front-init/main.py
@@ -8,7 +8,6 @@ import pathlib
 import socket
 import urllib.parse
 
-
 BASE_DIR = pathlib.Path()
 SERVER_HOST = '127.0.0.1'
 SERVER_PORT = 5000
@@ -19,6 +18,54 @@ def send_data_to_socket(data):
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     client_socket.sendto(data, (SERVER_HOST, SERVER_PORT))
     client_socket.close()
+
+
+def run_socket_server(ip, port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server = ip, port
+    sock.bind(server)
+    try:
+        while True:
+            data, address = sock.recvfrom(1024)
+            save_server_data(data)
+            print(f'Received data: {data.decode()} from: {address}')
+            sock.sendto(data, address)
+            print(f'Send data: {data.decode()} to: {address}')
+
+    except KeyboardInterrupt:
+        logging.info('Destroy server')
+    finally:
+        sock.close()
+
+
+def save_server_data(data):
+    parse_data = urllib.parse.unquote_plus(data.decode())
+    try:
+        dict_parse = {
+            str(datetime.datetime.now()): {key: value for key, value in
+                                           [el.split('=') for el in parse_data.split('&')]}}
+        with open(FILE_STORAGE, 'r+', encoding='utf-8') as f:
+            data_file = json.load(f)
+            new_record = {str(datetime.datetime.now()): dict_parse}
+            data_file.update(new_record)
+            with open(FILE_STORAGE, 'w', encoding='utf-8') as file:
+                json.dump(data_file, file, ensure_ascii=False, indent=4)
+
+    except ValueError as err:
+        logging.debug(f"for data {parse_data} error: {err}")
+    except OSError as err:
+        logging.debug(f"Write data {parse_data} error: {err}")
+
+
+def run_http_server():
+    server_address = ('0.0.0.0', HTTP_PORT)
+    http = HTTPServer(server_address, HTTPHandler)
+    try:
+        http.serve_forever()
+    except KeyboardInterrupt:
+        logging.info('Socket server stopped')
+    finally:
+        http.server_close()
 
 
 class HTTPHandler(BaseHTTPRequestHandler):
@@ -63,51 +110,6 @@ class HTTPHandler(BaseHTTPRequestHandler):
         with open(filename, "rb") as file:
             self.wfile.write(file.read())
 
-    def run_socket_server(ip, port):
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        server = ip, port
-        sock.bind(server)
-        try:
-            while True:
-                data, address = sock.recvfrom(1024)
-                save_server_data(data)
-                print(f'Received data: {data.decode()} from: {address}')
-                sock.sendto(data, address)
-                print(f'Send data: {data.decode()} to: {address}')
-
-        except KeyboardInterrupt:
-            logging.info('Destroy server')
-        finally:
-            sock.close()
-
-    def save_server_data(data):
-        parse_data = urllib.parse.unquote_plus(data.decode())
-        try:
-            dict_parse = {
-                str(datetime.datetime.now()): {key: value for key, value in
-                                               [el.split('=') for el in parse_data.split('&')]}}
-            with open(FILE_STORAGE.joinpath('storage/data.json'), 'r+', encoding='utf-8') as fd:
-                data_file = json.load(fd)
-                new_record = {str(datetime.datetime.now()): dict_parse}
-                data_file.update(new_record)
-                with open(FILE_STORAGE, 'w', encoding='utf-8') as file:
-                    json.dump(data_file, file, ensure_ascii=False, indent=4)
-
-        except ValueError as err:
-            logging.debug(f"for data {parse_data} error: {err}")
-        except OSError as err:
-            logging.debug(f"Write data {parse_data} error: {err}")
-
-    def run_http_server(self):
-        server_address = ('0.0.0.0', HTTP_PORT)
-        http = HTTPServer(server_address, HTTPHandler)
-        try:
-            http.serve_forever()
-        except KeyboardInterrupt:
-            logging.info('Socket server stopped')
-        finally:
-            http.server_close()
-
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG, format="%(threadName)s %(message)s")
@@ -117,7 +119,6 @@ if __name__ == '__main__':
     if not FILE_STORAGE.exists():
         with open(FILE_STORAGE, 'w', encoding='utf-8') as fd:
             json.dump({}, fd, ensure_ascii=False, indent=4)
-
     th_server = Thread(target=run_http_server)
     th_server.start()
 
